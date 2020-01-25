638f28df97f525355912eb1d8623074679af7c08
Sable <sablechilds@yahoo.com>
2020-01-24 23:49:01 -0500

    header-component-completed

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,33 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+    //create elements
+    const headerDiv = document.createElement('div')
+    const dateSpan = document.createElement('span')
+    const h1 = document.createElement('h1')
+    const tempSpan = document.createElement('span')
+
+    //add class names 
+    headerDiv.classList.add('header')
+    dateSpan.classList.add('date')
+    tempSpan.classList.add('temp')
+
+    //add child relationships
+    headerDiv.appendChild(dateSpan)
+    headerDiv.appendChild(h1)
+    headerDiv.appendChild(tempSpan)
+
+    //add text content
+    dateSpan.textContent = 'SMARCH 28, 2019';
+    h1.textContent = 'Lambda Times'
+    tempSpan.textContent = '98°'
+    return headerDiv; 
+}
+
+//create header
+const newHeader = Header();
+//select parent element
+const parentElement = document.querySelector('.header-container')
+// append to parent/add to DOM
+parentElement.appendChild(newHeader)
